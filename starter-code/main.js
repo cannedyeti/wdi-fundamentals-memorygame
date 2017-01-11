15286c54df56a6ceac11642ff39fdb06bd2cace0
Connor Potebnya <Connor Potebnya>
2017-01-11 12:28:23 -0800

    added comparison for cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -7,3 +7,45 @@ var cardTwo = "queen";
 var cardThree = "king";
 
 var cardFour = "king";
+
+if (cardTwo === cardFour) {
+	window.alert("You've found a match!");
+}
+else {
+	window.alert("Sorry, try again.");
+}
+
+if (cardOne === cardFour) {
+	window.alert("You've found a match!");
+}
+else {
+	window.alert("Sorry, try again.");
+}
+
+if (cardOne === cardThree) {
+	window.alert("You've found a match!");
+}
+else {
+	window.alert("Sorry, try again.");
+}
+
+if (cardTwo === cardThree) {
+	window.alert("You've found a match!");
+}
+else {
+	window.alert("Sorry, try again.");
+}
+
+if (cardThree === cardFour) {
+	window.alert("You've found a match!");
+}
+else {
+	window.alert("Sorry, try again.");
+}
+
+if (cardOne === cardTwo) {
+	window.alert("You've found a match!");
+}
+else {
+	window.alert("Sorry, try again.");
+}
