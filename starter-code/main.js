1e38c7338a61c2578fba0bde69753af9c78d0b8f
Connor Potebnya <Connor Potebnya>
2017-01-15 16:11:26 -0800

    added javascript to track cards matching or not matching. Added pictures to cards. Added a reset delay system.

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,63 +1,72 @@
 console.log("JS file is connected to HTML! Woo!")
 
-var cardOne = "queen";
+var cards = ["queen", "queen", "king", "king", "jack", "jack"]
+var cardsInPlay = [];
 
-var cardTwo = "queen";
-
-var cardThree = "king";
-
-var cardFour = "king";
-
-if (cardTwo === cardFour) {
-	window.alert("You've found a match!");
-}
-else {
-	window.alert("Sorry, try again.");
-}
-
-if (cardOne === cardFour) {
-	window.alert("You've found a match!");
-}
-else {
-	window.alert("Sorry, try again.");
-}
-
-if (cardOne === cardThree) {
-	window.alert("You've found a match!");
-}
-else {
-	window.alert("Sorry, try again.");
-}
-
-if (cardTwo === cardThree) {
-	window.alert("You've found a match!");
-}
-else {
-	window.alert("Sorry, try again.");
-}
-
-if (cardThree === cardFour) {
-	window.alert("You've found a match!");
-}
-else {
-	window.alert("Sorry, try again.");
-}
-
-if (cardOne === cardTwo) {
-	window.alert("You've found a match!");
-}
-else {
-	window.alert("Sorry, try again.");
-}
 
 var createCards = function(){
-	var board = document.getElementById('game-board');
-		for (i = 0; i < 4; i++) {
+	var gameBoard = document.getElementById('game-board');
+		for (i = 0; i < cards.length; i++) {
 			var newCard = document.createElement('div');
-			board.appendChild(newCard);
+			newCard.setAttribute('data-card', cards[i]);
+			gameBoard.appendChild(newCard);
 			newCard.setAttribute('class', 'card');
+			newCard.addEventListener('click', isTwoCards);
+
 		}
 	}
 
+
+var isTwoCards = function() 
+{
+    var dataCard = this.getAttribute('data-card');
+    cardsInPlay.push(dataCard);
+    if (dataCard === 'king') 
+    {
+        this.innerHTML = '<img src="http://image.shutterstock.com/display_pic_with_logo/10795/10795,1184461084,2/stock-vector-king-of-clubs-playing-card-illustration-vector-3867085.jpg" alt="King" />'; // King
+    } else if (dataCard === 'queen')
+    {
+        this.innerHTML = '<img src="https://s-media-cache-ak0.pinimg.com/originals/f1/3e/55/f13e55f7e734346748bf45b03465f403.jpg" alt="Queen" />'; // Queen
+    } else
+    {
+        this.innerHTML = '<img src="http://previews.123rf.com/images/mannaggia/mannaggia0911/mannaggia091100006/5957156-Jack-of-hearts-playing-card-Stock-Vector-cards.jpg" alt="Jack" />'; // Joker
+    }
+    if (cardsInPlay.length === 2)
+    {
+        isMatch(cardsInPlay);
+
+    }
+}
+
+
+
+var isMatch = function(cardsInPlay)
+{
+    if (cardsInPlay[0] === cardsInPlay[1])
+    {
+        setTimeout(function(){ alert("You did it!"); }, 200);
+        clearDelay();
+
+    } else 
+    {
+        setTimeout(function(){ alert("Try again.."); }, 200);
+        clearDelay();
+    }
+}
+
+
+var clearboard = function() {
+	var gameBoard = document.getElementById('game-board')
+	while (gameBoard.firstChild) {
+    gameBoard.removeChild(gameBoard.firstChild);
+	}
+    createCards();
+    cardsInPlay = [];
+}
+var clear;
+
+function clearDelay() {
+	clear = setTimeout(clearboard, 2000);
+}
 
 createCards();
